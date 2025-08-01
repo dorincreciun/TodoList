d6d422eff269ee23a159b53cc6cfb94f7a3144ba
dorincreciun <creciundorin25@gmail.com>
2025-08-01 09:06:30 +0300

    Add Deploy settings for CI/CD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
+  root: __dirname,
+  base: '/TodoList/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
