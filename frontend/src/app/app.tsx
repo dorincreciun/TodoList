be9fac6fb9eb84edcb9f8569522dc48103c915b9
Creciun <creciundorin25@gmail.com>
2025-08-02 13:26:24 +0300

    refactor: simplify styles and auth guard logic
    - Updated `index.css` to apply root-level styles with Tailwind.
    - Removed unused `useAuthStore` and replaced `isAuthorized` with a default value in `AuthGuard`.
    - Simplified `AppLayout` by removing unnecessary effects, `Header`, and `Footer`.

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,26 +1,13 @@
-import {StrictMode, useEffect} from "react";
+import {StrictMode} from "react";
 import {BrowserRouter} from "react-router";
 import {Router} from "./routing";
-/* Features */
-import {useAuthStore} from "../features/auth";
-/* Shared -> Common */
-import {Header} from "../shared/common/Header";
-import {Footer} from "../shared/common/Footer";
 
 export const AppLayout = () => {
-    const {update} = useAuthStore()
-
-    /* refresh accessToken */
-    useEffect(() => {
-        update();
-    }, []);
 
     return (
         <StrictMode>
             <BrowserRouter>
-                <Header/>
                 <Router/>
-                <Footer/>
             </BrowserRouter>
         </StrictMode>
     )
--- frontend/src/app/guards/AuthGuard.tsx
+++ frontend/src/app/guards/AuthGuard.tsx
@@ -1,9 +1,8 @@
 import {Navigate, Outlet} from "react-router";
-import {useAuthStore} from "../../features/auth";
 import {Routing_Paths} from "../config";
 
 export const AuthGuard = () => {
-    const isAuthorized = useAuthStore((state) => state.isAuthorized)
+    const isAuthorized = false
     if(!isAuthorized){
         return <Navigate to={Routing_Paths.LOGIN} replace />
     }
