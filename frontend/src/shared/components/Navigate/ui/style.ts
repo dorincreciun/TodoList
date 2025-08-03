b260461ac795e9cf476a0aea0216e717c6bbf1d8
Creciun <creciundorin25@gmail.com>
2025-08-03 17:50:12 +0300

    refactor(ui): adjust padding in Navigate component styles

--- frontend/src/shared/components/Navigate/ui/style.ts
+++ frontend/src/shared/components/Navigate/ui/style.ts
@@ -2,7 +2,7 @@ import {cva, type VariantProps} from "class-variance-authority";
 
 export type NavigateVariant = VariantProps<typeof style>;
 export const style = cva([
-    'p-3.5 rounded-xl',
+    'p-3 rounded-xl',
     'text-[#5F6388]',
     'transition-colors duration-200',
     'hover:bg-[#5051F9]/50 text-white/50'
