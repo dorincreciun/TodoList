10091fc630db0f623c536f0de9cf3b8e5ba901fc
dorincreciun <creciundorin25@gmail.com>
2025-08-04 16:03:53 +0300

    refactor(frontend): improve Header component structure with added comments for clarity

--- frontend/src/shared/common/Header/ui/index.tsx
+++ frontend/src/shared/common/Header/ui/index.tsx
@@ -6,16 +6,18 @@ import {Button} from "../../../components/Button";
 export const Header = () => {
     return (
         <header className={'max-w-full w-full bg-[#1E1F25] flex items-center h-[76px] px-10'}>
-
             <div className={'flex items-center mx-auto relative max-w-[450px] w-full'}>
+
+                {/* Search input */}
                 <Input
                     type={'text'}
                     iconEnd={<Search/>}
                     placeholder={'Search anything...'}
                 />
             </div>
-
             <div className={'flex items-center space-x-3'}>
+
+                {/* Notification */}
                 <Button
                     rounded={'xl'}
                     className={'p-1'}
@@ -23,6 +25,8 @@ export const Header = () => {
                 >
                     <Bell />
                 </Button>
+
+                {/* User control */}
                 <Button
                     rounded={'xl'}
                     className={'p-1'}
