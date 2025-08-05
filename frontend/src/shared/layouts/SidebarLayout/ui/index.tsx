9bf835396531db1c7f67a201a0dc3b4721242a33
dorincreciun <creciundorin25@gmail.com>
2025-08-05 12:03:31 +0300

    refactor(frontend): restructure SidebarLayout UI for better scroll and layout management

--- frontend/src/shared/layouts/SidebarLayout/ui/index.tsx
+++ frontend/src/shared/layouts/SidebarLayout/ui/index.tsx
@@ -16,14 +16,16 @@ export const SidebarLayout = ({isOpen, render, onClickOutside}: SidebarProps & {
         <aside
             ref={ref}
             className={cn(
-                "fixed right-0 top-[76px] bg-[#1E1F25] h-full max-sm:w-screen sm:w-[500px] flex items-center justify-center transition-transform duration-500 ease-in-out shadow-xl p-8",
+                "fixed right-0 top-[76px] bg-[#1E1F25] transition-transform duration-500 ease-in-out shadow-xl",
                 {
                     "translate-x-0": isOpen,
                     "translate-x-full": !isOpen,
                 }
             )}
         >
-            {render({isOpen})}
+            <div className={'size-full overflow-y-auto max-sm:w-screen sm:w-[500px] flex items-center justify-center p-8'}>
+                {render({isOpen})}
+            </div>
         </aside>
     );
 };
