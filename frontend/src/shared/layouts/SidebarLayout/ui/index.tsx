c14eabf6c14c1c7261d85b6d06f1828a3489a2df
dorincreciun <creciundorin25@gmail.com>
2025-08-05 13:19:18 +0300

    style(frontend): improve global height styling and enhance SidebarLayout responsiveness

--- frontend/src/shared/layouts/SidebarLayout/ui/index.tsx
+++ frontend/src/shared/layouts/SidebarLayout/ui/index.tsx
@@ -16,16 +16,16 @@ export const SidebarLayout = ({isOpen, render, onClickOutside}: SidebarProps & {
         <aside
             ref={ref}
             className={cn(
-                "fixed right-0 top-[76px] bg-[#1E1F25] transition-transform duration-500 ease-in-out shadow-xl",
+                "fixed right-0 top-[76px] h-[calc(100dvh-76px)] bg-[#1E1F25] shadow-xl transition-transform duration-500 ease-in-out",
+                "overflow-y-auto flex items-center justify-center p-8",
+                "w-full sm:max-w-[500px]",
                 {
                     "translate-x-0": isOpen,
                     "translate-x-full": !isOpen,
                 }
             )}
         >
-            <div className={'size-full overflow-y-auto max-sm:w-screen sm:w-[500px] flex items-center justify-center p-8'}>
-                {render({isOpen})}
-            </div>
+            {render({ isOpen })}
         </aside>
     );
 };
